913f479035929b4bc725a944945e745b75459caf
Alyona Uskova <a-uskova@inbox.ru>
2021-09-30 14:23:26 +0400

    Main added

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 2_000_000_000;
+        int transferSum = 500_000_000;
+        int total = amount + transferSum;
+        System.out.println(total);
+    }
+}
